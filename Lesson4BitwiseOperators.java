308cbc67599ea8e9e0fbc48c3e4d9b7c51c5a73c
Partho Probal <84497748+ParthoProbal@users.noreply.github.com>
2024-06-07 19:33:44 +0600

    Add files via upload

---
+++ Lesson4BitwiseOperators.java
@@ -0,0 +1,29 @@
+public class Lesson4BitwiseOperators {
+
+    public static void main(String[] args) {
+
+        // Bitwise AND --> &
+
+        boolean p = false;
+        boolean q = false;
+
+        System.out.println(p & q);
+
+        // Bitwise inclusive OR --> |
+
+        p = false;
+        q = false;
+
+        System.out.println(p | q);
+
+        // Bitwise exclusive OR --> ^
+
+        p = true;
+        q = false;
+
+        System.out.println(p ^ q);
+
+        // All the outputs will work according to the truth table
+
+    }
+}
