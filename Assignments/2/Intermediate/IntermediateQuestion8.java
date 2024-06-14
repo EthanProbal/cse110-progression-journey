fdd7c9f95773a2b35033fbb8ae4413b84ed25432
Partho Probal <84497748+ParthoProbal@users.noreply.github.com>
2024-06-14 20:11:32 +0600

    Add files via upload

---
+++ Assignments/2/Intermediate/IntermediateQuestion8.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class IntermediateQuestion8 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = 2;
+        int b = 5;
+        int c = 8;
+
+        int result = (2 * b * (c - a) / 3) + 7;
+
+        System.out.println(result);
+
+    }
+}
