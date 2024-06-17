351dd927c7da18de3d6bc8612c56d728e06481e1
Partho Probal <84497748+ParthoProbal@users.noreply.github.com>
2024-06-17 12:40:43 +0600

    Add files via upload

---
+++ Assignments/2/Intermediate/IntermediateQuestion9.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class IntermediateQuestion9 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // CodeBlock 1
+        int n = 6;
+        int m = 1;
+        int result = (n * m);
+        System.out.printf("%d X %d = %d\n", n, m++, result);
+
+        // CodeBlock 2
+        result = (n * m);
+        System.out.printf("%d X %d = %d\n", n, m++, result);
+
+        // CodeBlock 3
+        result = (n * m);
+        System.out.printf("%d X %d = %d\n", n, m++, result);
+
+        // CodeBlock 4
+        result = (n * m);
+        System.out.printf("%d X %d = %d\n", n, m++, result);
+
+        // CodeBlock 5
+        result = (n * m);
+        System.out.printf("%d X %d = %d\n", n, m++, result);
+
+        // CodeBlock 6
+        result = (n * m);
+        System.out.printf("%d X %d = %d\n", n, m++, result);
+
+        // CodeBlock 7
+        result = (n * m);
+        System.out.printf("%d X %d = %d\n", n, m++, result);
+
+        // CodeBlock 8
+        result = (n * m);
+        System.out.printf("%d X %d = %d\n", n, m++, result);
+
+        // CodeBlock 9
+        result = (n * m);
+        System.out.printf("%d X %d = %d\n", n, m++, result);
+
+        // CodeBlock 10
+        result = (n * m);
+        System.out.printf("%d X %d = %d\n", n, m++, result);
+
+    }
+}
