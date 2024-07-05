35301899d890a588f30fa916a6d2f2e29522aa63
Partho Probal <84497748+ParthoProbal@users.noreply.github.com>
2024-07-05 12:45:04 +0600

    Add files via upload

---
+++ Assignments/4/Beginner/BeginnerQuestion1_b.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BeginnerQuestion1_b {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int start = -10;
+        int end = 20;
+
+        while (start < end) {
+            System.out.printf("%d, ", start);
+            start += 5;
+        }
+        System.out.printf("%d", end);
+
+    }
+}
