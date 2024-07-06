99d9054d8aa5b9a5722c7a5fa785534b33475218
Partho Probal <84497748+ParthoProbal@users.noreply.github.com>
2024-07-06 23:05:00 +0600

    Update BeginnerQuestion1_a.java

--- Assignments/4/Beginner/BeginnerQuestion1_a.java
+++ Assignments/4/Beginner/BeginnerQuestion1_a.java
@@ -8,11 +8,15 @@ public class BeginnerQuestion1_a {
         int start = 24;
         int end = -6;
 
-        while (start > end) {
-            System.out.printf("%d, ", start);
-            start -= 6;
+        while (true) {
+            if (start > end) {
+                System.out.printf("%d, ", start);
+                start -= 6;
+            } else if (start <= end) {
+                System.out.printf("%d", start);
+                break;
+            }
         }
-        System.out.printf("%d", end);
 
     }
 }
