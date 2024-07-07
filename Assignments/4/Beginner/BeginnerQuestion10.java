082c140be720bfddb1cb8600e24521b98fb09c12
Partho Probal <84497748+ParthoProbal@users.noreply.github.com>
2024-07-07 14:52:56 +0600

    Update BeginnerQuestion10.java

--- Assignments/4/Beginner/BeginnerQuestion10.java
+++ Assignments/4/Beginner/BeginnerQuestion10.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BeginnerQuestion10 {
+public class BeginnerQuestion11_a {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,11 +8,19 @@ public class BeginnerQuestion10 {
         System.out.printf("Enter number: ");
         int num = sc.nextInt();
         String numLength = "" + num;
+        int tempNum = num;
+
+        System.out.println(numLength);
 
         int[] digit = new int[numLength.length()];
         int i = 0;
 
         while (true) {
+            if (num < 0) {
+                num = num * (-1);
+                // digit[numLength.length() - 1] = num / (-1) * (int) Math.pow(10,
+                // (numLength.length() - 1));
+            }
             digit[i] = (num % 10);
             i++;
             num /= 10;
@@ -22,7 +30,16 @@ public class BeginnerQuestion10 {
             }
         }
 
+        num = tempNum;
+
+        if (digit[numLength.length() - 1] == 0) {
+            digit[numLength.length() - 1] = -digit[numLength.length() - 2];
+        }
+
         for (int j = digit.length - 1; j > 0; j--) {
+            if (j == digit.length - 2 && num < 0) {
+                continue;
+            }
             System.out.printf("%d, ", digit[j]);
         }
         System.out.printf("%d", digit[0]);
