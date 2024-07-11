2a02714c1d5d8566e963d3f21620eabd46be9f32
Partho Probal <84497748+ParthoProbal@users.noreply.github.com>
2024-07-11 16:17:08 +0600

    Add files via upload

---
+++ Website Problem Solving/CSE110 Quiz/CSE110_Lab_Quiz_04_Set_A.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class CSE110_Lab_Quiz_04_Set_A {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("Enter heart rate: ");
+        int num = sc.nextInt();
+
+        // Found new Technique to Find the Highest and Lowest value inside a Loop
+
+        int tempMax = Integer.MIN_VALUE;
+        int tempMin = Integer.MAX_VALUE;
+        int beats = 0;
+        int sum = 0;
+
+        for (int i = 1; i <= num; i++) {
+            System.out.printf("Enter heart rate %d: ", i);
+            beats = sc.nextInt();
+            sum += beats;
+
+            if (beats > tempMax) {
+                tempMax = beats;
+            } else if (beats < tempMax) {
+                tempMax = tempMax;
+            }
+
+            if (beats < tempMin) {
+                tempMin = beats;
+            } else if (beats > tempMin) {
+                tempMin = tempMin;
+            }
+        }
+
+        float avg = (float) sum / (float) num;
+        System.out.printf("Heart rate range: %d - %d\n", tempMin, tempMax);
+        System.out.printf("Average heart rate: %f\n", avg);
+    }
+}
