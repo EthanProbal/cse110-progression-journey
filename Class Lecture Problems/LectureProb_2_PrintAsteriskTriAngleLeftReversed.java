609329428e9b973c4343caa8d2c0758406c9382b
Partho Probal <84497748+ParthoProbal@users.noreply.github.com>
2024-09-04 23:42:57 +0600

    Update LectureProb_2_PrintAsteriskTriAngleLeftReversed.java

--- Class Lecture Problems/LectureProb_2_PrintAsteriskTriAngleLeftReversed.java
+++ Class Lecture Problems/LectureProb_2_PrintAsteriskTriAngleLeftReversed.java
@@ -1,8 +1,14 @@
 public class LectureProb_2_PrintAsteriskTriAngleLeftReversed {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 5 - i; j++) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= i; k++) {
                 System.out.print("*");
             }
             System.out.println();
