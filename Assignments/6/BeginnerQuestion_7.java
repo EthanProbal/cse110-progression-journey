2bf9ff3848ae3ad8d6afb86167498042a055c0bd
Partho Probal <84497748+ParthoProbal@users.noreply.github.com>
2024-09-16 14:39:06 +0600

    Update BeginnerQuestion_7.java

--- Assignments/6/BeginnerQuestion_7.java
+++ Assignments/6/BeginnerQuestion_7.java
@@ -10,32 +10,21 @@ public class BeginnerQuestion_7 {
         System.out.print("Enter String 2: ");
         StringBuilder str2 = new StringBuilder(sc.nextLine());
 
-        for (int i = 0; i < str1.length(); i++) {
-            for (int j = 0; j < str2.length(); j++) {
-                if (str2.charAt(j) == str1.charAt(i)) {
-                    str2.delete(j, j + 1);
-                    str1.delete(i, i + 1);
-                    i = 0;
-                    j = 0;
+        for (int k = 0; k < 2; k++) {
+            for (int i = 0; i < str1.length(); i++) {
+                for (int j = 0; j < str2.length(); j++) {
+                    if (str2.charAt(j) == str1.charAt(i)) {
+                        str2.delete(j, j + 1);
+                        str1.delete(i, i + 1);
+                        i = 0;
+                        j = 0;
+                    }
+
                 }
-
-            }
-        }
-
-        for (int i = 0; i < str1.length(); i++) {
-            for (int j = 0; j < str2.length(); j++) {
-                if (str2.charAt(j) == str1.charAt(i)) {
-                    str2.delete(j, j + 1);
-                    str1.delete(i, i + 1);
-                    i = 0;
-                    j = 0;
-                }
-
             }
         }
 
         String finalStr = str1.toString().toUpperCase() + str2.toString().toUpperCase();
-
         System.out.println(finalStr);
 
     }
