7421a87bb32e1c121799f0bad0b74e82e4c7472a
Partho Probal <84497748+ParthoProbal@users.noreply.github.com>
2024-09-21 20:03:15 +0600

    Add files via upload

---
+++ Assignments/7/BeginnerQuestion_1_b.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class BeginnerQuestion_1_b {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("N = ");
+        int arrLen = sc.nextInt();
+
+        int num[] = new int[arrLen];
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.printf("Enter a Number: ");
+            num[i] = sc.nextInt();
+        }
+
+        System.out.printf("The Elements of the Array are: \n");
+        for (int i = 0; i < num.length; i++) {
+            System.out.printf("%d: %d\n", i, num[i]);
+        }
+
+        System.out.printf("Enter Another Number: ");
+        int anotherNum = sc.nextInt();
+        int num2[] = new int[arrLen + 1];
+
+        for (int i = 0; i < num.length; i++) {
+            num2[i] = num[i];
+        }
+
+        num2[arrLen] = anotherNum;
+
+        System.out.printf("After Resizing the array: \n");
+        for (int i = 0; i < num2.length; i++) {
+            System.out.printf("%d ", num2[i]);
+        }
+    }
+}
